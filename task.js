3211d5d3f140bbb4e3076c050cb5a0910fe14c20
dhatchan96 <dhatchan0@gmail.com>
2024-02-04 10:07:51 +0530

    Task 1

---
+++ task.js
@@ -0,0 +1,29 @@
+/* 1. Write a blog on Difference between HTTP1.1 vs HTTP2
+=> In a nutshell, the shift from HTTP/1.1 to HTTP/2 brings crucial improvements to web communication:
+
+        Multiplexing: HTTP/2 enables concurrent data transfer, reducing latency by allowing multiple requests to be processed simultaneously over a single connection.
+
+        Header Compression: Unlike HTTP/1.1, HTTP/2 uses compressed headers, minimizing data transfer overhead and accelerating load times.
+
+        Binary Protocol: HTTP/2 adopts a binary format for more efficient parsing, reducing errors during transmission and enhancing data compression.
+
+        Server Push: This feature allows servers to proactively send resources to clients, optimizing load times by anticipating and fulfilling potential requests.
+
+        Prioritization: HTTP/2 introduces advanced prioritization, enabling clients to specify the importance of resources for faster and more efficient processing.
+
+        The transition to HTTP/2 signifies a quantum leap in web performance, offering a more streamlined, faster, and efficient browsing experience. */
+
+
+/* 2. Write a blog about objects and its internal representation in Javascript
+
+
+=> JavaScript objects, fundamental to the language's versatility, hold key-value pairs and are structured internally by property descriptors and a prototype chain. Understanding these internals is vital for effective manipulation.
+
+Property Descriptors: Define attributes like writability and enumerability.
+Prototype Chain: Enables inheritance for cleaner, reusable code.
+Creating objects varies:
+
+Object Literal: Simple and quick.
+Constructor Functions: Structured blueprints.
+Object.create(): Prototype-based object creation.
+JavaScript's prototypal inheritance enhances flexibility. Armed with this knowledge, developers unlock the potential to create efficient, modular, and scalable JavaScript applications. */
